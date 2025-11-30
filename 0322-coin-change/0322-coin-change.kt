780f1fba14404f280203497453c3e4600e0e31b5
Thanh Pham <phamnoone@gmail.com>
2025-11-30 22:59:51 +0900

    Time: 30 ms (34.09%), Space: 46.4 MB (35.23%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        if (amount == 0) return 0
+
+        var amounts = IntArray(amount+1)
+        amounts[0] = 0
+        for (i in 1..amount)
+            amounts[i] = amount + 1
+
+
+        for (coin in coins)
+            for (i in coin..amount)
+                amounts[i] = min(amounts[i], 1 + amounts[i - coin])
+
+        return if (amounts.last() == amount+1) -1 else amounts.last()
+    }
+}
