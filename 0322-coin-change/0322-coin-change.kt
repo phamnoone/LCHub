d9f4f57e373f5f3e3fe2f985645b21301adb88fa
Thanh Pham <phamnoone@gmail.com>
2025-11-30 23:08:59 +0900

    Time: 16 ms (83.86%), Space: 46.4 MB (35.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.kt
+++ 0322-coin-change/0322-coin-change.kt
@@ -7,10 +7,11 @@ class Solution {
         for (i in 1..amount)
             amounts[i] = amount + 1
 
-
+        // if i == coin then 1 + amounts[i - coin] always = 1 because d[0] = 0
         for (coin in coins)
             for (i in coin..amount)
                 amounts[i] = min(amounts[i], 1 + amounts[i - coin])
+           
 
         return if (amounts.last() == amount+1) -1 else amounts.last()
     }
