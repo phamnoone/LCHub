638c95085260dfc3ca5447e7b8759b74d09a9d47
Thanh Pham <phamnoone@gmail.com>
2025-12-01 23:47:06 +0900

    Time: 0 ms (100%), Space: 41.4 MB (18.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.kt
+++ 0198-house-robber/0198-house-robber.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun rob(nums: IntArray): Int {
         if (nums.size == 1) return nums[0]
-        if (nums.size == 2) return nums.max()
+        if (nums.size == 2) return max(nums[0], nums[1])
 
         val moneys = IntArray(nums.size)
         moneys[0] = nums[0]
