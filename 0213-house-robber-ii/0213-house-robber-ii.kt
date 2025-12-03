da3688f07bda9d1d5de09c02ec537fdc3a7d029e
Thanh Pham <phamnoone@gmail.com>
2025-12-03 15:05:40 +0900

    Time: 0 ms (100%), Space: 40.1 MB (98.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.kt
+++ 0213-house-robber-ii/0213-house-robber-ii.kt
@@ -2,24 +2,21 @@ class Solution {
     fun rob(nums: IntArray): Int {
         if (nums.size == 1) return nums[0]
         return  max(
-            robI(nums.sliceArray(1..nums.size-1)), 
-            robI(nums.sliceArray(0..nums.size - 2))
+            robI(nums, 1, nums.size-1), 
+            robI(nums, 0, nums.size - 2)
         )
     }
 
-    fun robI(nums: IntArray): Int {
-        if (nums.size == 1) return nums[0]
-        if (nums.size == 2) return max(nums[0], nums[1])
-
-        val moneys = IntArray(nums.size)
-        moneys[0] = nums[0]
-        moneys[1] = max(nums[0], nums[1])
-        moneys[2] = nums[0] + nums[2]
-
-        for (i in 3..(nums.size -1)) {
-            moneys[i] = max(moneys[i-2], moneys[i-3]) + nums[i]
+    fun robI(nums: IntArray, star: Int, end: Int): Int {
+        var rob2 = 0
+        var rob1 = 0 
+        
+        for (i in star..end) {
+            val temp = max(nums[i] + rob2, rob1)
+            rob2 = rob1
+            rob1 = temp
         }
 
-        return max(moneys[nums.size -1], moneys[nums.size -2])
+        return rob1
     }
 }
